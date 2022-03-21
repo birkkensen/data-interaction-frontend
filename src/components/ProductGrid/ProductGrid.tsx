8aa4c4e6ee5e726d61d49a82b5ca34eb10578e87
Birk <birk.kensen@gmail.com>
2022-03-21 17:46:32 +0100

    did my first styling with tailwindcss - epic

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
-import "./index.css";
 import { Product } from "../../interfaces";
 const ProductGrid: React.FC<Product> = ({ _id, name, description, image, price }) => {
   return (
-    <Link className="product-link" to={`product/${_id}`}>
-      <img className="product-image" src={image} alt={name} />
-      <div className="product-details">
-        <h4 className="product-details-title">{name}</h4>
-        <p className="product-details-desc">{description}</p>
-        <p className="product-details-price">{price} kr</p>
+    <Link className="group" to={`product/${_id}`}>
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <img
+          className="w-full h-full object-center object-cover group-hover:opacity-75"
+          src={image}
+          alt={name}
+        />
       </div>
+      <h4 className="mt-4 text-sm text-gray-700">{name}</h4>
+      <p className="mt-1 text-sm text-gray-700">{description}</p>
+      <p className="mt-1 text-lg font-medium text-gray-900">{price} kr</p>
     </Link>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { ProductGrid } from "../../components";
 import { getAllProducts } from "../../api";
-import "./index.css";
+// import "./index.css";
 import { Product } from "../../interfaces";
 
 const Home: React.FC = (): JSX.Element => {
@@ -16,22 +16,26 @@ const Home: React.FC = (): JSX.Element => {
   return (
     <>
       <FeaturedImage />
-      <h2 style={{ margin: "20px" }}>Some stuff</h2>
-      <section className="product-grid-container">
-        {products &&
-          products.map((product) => {
-            return (
-              <ProductGrid
-                key={uuidv4()}
-                name={product.name}
-                description={product.description}
-                image={product.image}
-                price={product.price}
-                _id={product._id}
-              />
-            );
-          })}
-      </section>
+      <div className="bg-white">
+        <div className="max-w-2x1 mx-auto py-16 px-14 sm:py-24 sm:px-6 lg:max-w-7x1 lg:px-8">
+          <h2 className="sr-only">Some stuff</h2>
+          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products &&
+              products.map((product) => {
+                return (
+                  <ProductGrid
+                    key={uuidv4()}
+                    name={product.name}
+                    description={product.description}
+                    image={product.image}
+                    price={product.price}
+                    _id={product._id}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
